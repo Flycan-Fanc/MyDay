52ff53b2a13c27eef14f4ad0d55dcce6c6c4abeb
Flycan-Fanc <flycanfanc@outlook.com>
2025-05-11 11:48:23 +0800

    编写了syncMeta相关api

--- src/renderer/src/utils/api/modules/syncMeta.js
+++ src/renderer/src/utils/api/modules/syncMeta.js
@@ -4,4 +4,22 @@
 
 import httpClient from "../httpClient";
 
-export const addSyncMeta = (data) => httpClient.post("/syncMeta/add", data);
+// 通知服务器添加同步元数据
+export const addSyncMeta = (userId) => {
+  return httpClient.post(`/syncMeta/add/${userId}`)
+}
+
+// 获取同步元数据
+export const getSyncMeta = (userId) => {
+  return httpClient.get(`/syncMeta/${userId}`)
+}
+
+// 通知服务器更新同步元数据
+export const updateSyncMeta = (userId) => {
+  return httpClient.post(`/syncMeta/update/${userId}`)
+}
+
+// 通知服务器删除同步元数据
+export const deleteSyncMeta = (userId) => {
+  return httpClient.delete(`/syncMeta/${userId}`)
+}
